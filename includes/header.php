94766a831638b8a84fedcc5375e93941b42cc1f3
willyhunt <benjamin.lebotland@gmail.com>
2017-02-17 22:03:57 +0100

    1st commit containing: 	-Folder architechture 	-bootstrap example form a blog 	-splitted into header.php / footer.php / sidebar.php / index.php

---
+++ includes/header.php
@@ -0,0 +1,37 @@
+ <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Blog OCR TP3</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+    
+ 
+    <!-- Custom styles for this template -->
+    <link href="css/custom.css" rel="stylesheet" type="text/css"/>
+
+  </head>
+
+  <body>
+
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="blog-nav">
+          <a class="blog-nav-item active" href="#">Home</a>
+          <a class="blog-nav-item" href="#">New features</a>
+          <a class="blog-nav-item" href="#">Press</a>
+          <a class="blog-nav-item" href="#">New hires</a>
+          <a class="blog-nav-item" href="#">About</a>
+        </nav>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="blog-header">
+        <h1 class="blog-title">The Bootstrap Blog</h1>
+        <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+      </div>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include("includes/header.php"); ?>
+
+<div class="row">
+    <div class="col-sm-8 blog-main">
+        <div class="blog-post">
+            <h2 class="blog-post-title">Sample blog post</h2>
+            <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+
+        </div><!-- /.blog-post -->
+
+        <div class="blog-post">
+            <h2 class="blog-post-title">Another blog post</h2>
+            <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
+        </div><!-- /.blog-post -->
+
+        <nav>
+          <ul class="pager">
+            <li><a href="#">Previous</a></li>
+            <li><a href="#">Next</a></li>
+          </ul>
+        </nav>
+
+    </div><!-- /.blog-main -->
+
+  <?php include 'includes/sidebar.php';?>
+  
+    </div><!-- /.row -->
+</div><!-- /.container -->
+
+<?php include 'includes/footer.php';?>
